a0153e549f87383d0493621841012135c630d3b0
Kamol Mavlonov <kamoljan@gmail.com>
2017-07-28 13:08:38 +0900

    Add hub

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@
 az group create --name $KHUB_ENV_NAME --location $AZURE_LOCATION
 
 az group deployment create \
-    --name $KHUB_ENV_NAME_DEPLOYMENT \
+    --name $KHUB_ENV_NAME \
     --resource-group $KHUB_ENV_NAME \
     --template-uri https://raw.githubusercontent.com/kamoljan/azure-hub-spoke/master/main.json \
     --parameters ./main.parameters.json
---
+++ scripts/destroy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+az group delete -n ne-khub-dev -y
+az group delete -n ne-kauth-dev -y
