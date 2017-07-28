edb65a326b27802b1fe8f3ee91ed3a4f53af5d34
Kamol Mavlonov <kamoljan@gmail.com>
2017-07-28 13:15:02 +0900

    Remove comments from JSON

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 az group deployment validate \
-    --name $KHUB_ENV_NAME_DEPLOYMENT \
+    --name $KHUB_ENV_NAME \
     --resource-group $KHUB_ENV_NAME \
-    --template-file main.json
-    --parameters @main.parameters.json
+    --template-uri https://raw.githubusercontent.com/kamoljan/azure-hub-spoke/master/main.json \
+    --parameters ./main.parameters.json
 
