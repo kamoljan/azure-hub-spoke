57b66ca9a481f310802f3213d5d0db11f4e9fce3
Kamol Mavlonov <kamoljan@gmail.com>
2017-07-30 18:39:19 +0900

    Following azure template

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -3,8 +3,8 @@
 az group deployment validate \
     --name $HUB_ENV_NAME \
     --resource-group $HUB_ENV_NAME \
-    --template-file main.json \
-    --parameters @main.parameters.json
-    # --template-uri https://raw.githubusercontent.com/kamoljan/azure-hub-spoke/master/main.json \
-    # --parameters ./main.parameters.json
+    --template-file azuredeploy.json \
+    --parameters @azuredeploy.parameters.json
+    # --template-uri https://raw.githubusercontent.com/kamoljan/azure-hub-spoke/master/azuredeploy.json \
+    # --parameters ./azuredeploy.parameters.json
 
